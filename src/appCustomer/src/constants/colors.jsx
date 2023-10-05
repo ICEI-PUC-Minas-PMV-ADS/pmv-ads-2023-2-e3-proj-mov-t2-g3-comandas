98457623da2cba8294816d1a03fc83734f5ea288
creinis <115181527+creinis@users.noreply.github.com>
2023-10-05 11:24:16 +0200

    add components: buttom Large; COLORS

---
+++ src/appCustomer/src/constants/colors.jsx
@@ -0,0 +1,20 @@
+const COLORS = {
+  primary: '#F35C56',
+
+  accentBlue: '#272459',
+  accentDarkGreen: '#B3FEAF',
+  accentLightGreen: '#59FF1F',
+
+  neutralBlue: '#75759E',
+  neutralMiddGrey: '#C8C8D3',
+  neutralLightGrey: '#F0F1F5',
+  neutrlWhite: '#FFFFFF',
+
+  semanticYellow: '#F7D96F',
+  semanticPurple: '#BBAFFE',
+  semanticMagenta: '#F6BDAC',
+  semanticBlue: '#89E2FE',
+  semanticBrown: '#F5B888',
+};
+
+export default COLORS;
