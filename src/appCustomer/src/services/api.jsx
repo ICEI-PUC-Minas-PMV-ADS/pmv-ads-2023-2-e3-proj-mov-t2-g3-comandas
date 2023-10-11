d6ce784212676e726895852c8b650e6620488fc6
creinis <115181527+creinis@users.noreply.github.com>
2023-10-11 10:03:58 +0200

    Update api.jsx

--- src/appCustomer/src/services/api.jsx
+++ src/appCustomer/src/services/api.jsx
@@ -2,148 +2,22 @@ import axios from 'axios';
 import { Alert } from 'react-native';
 import { getUser, navigate, deleteUser } from '../utils';
 
+const BASE_URL = 'https://comandas-api.vercel.app/docs/';
+
 const api = axios.create({
 
-  baseURL: 'https://comandas-api.vercel.app/docs/',
+/* const headers = {
+    'x-api-key': process.env.API_KEY,
+    Authorization: process.env.ADMIN_TOKEN,
+}; */
 
-  headers: {
-
-    'Content-Type': 'application/json',
-
-    Accept: 'application/json',
-
-  },
-
+export const api = axios.create({
+  baseURL: BASE_URL,
+  headers: { 'Content-Type': 'application/json' },
+  withCredentials: true,
+  Accept: 'application/json',
 });
 
-
-api.interceptors.response.use(
-
-  (response) =>
-
-    // Do something with response data
-
-
-
-    response,
-
-
-
-  (error) => {
-
-    // Do something with response error
-
-
-
-    // You can even test for a response code
-
-
-
-    // and try a new request before rejecting the promise
-
-
-
-    if (
-
-      error.request._hasError === true && error.request._response.includes('connect')
-
-    ) {
-
-      Alert.alert(
-
-        'Aviso',
-
-
-
-        'Não foi possível conectar aos nossos servidores, sem conexão a internet',
-
-
-
-        [{ text: 'OK' }],
-
-
-
-        { cancelable: false },
-
-      );
-
-    }
-
-
-
-    if (error.response.status === 401) {
-
-      const requestConfig = error.config;
-
-
-
-      // O token JWT expirou
-
-
-
-      deleteUser().then(() => {
-
-        navigate('AuthLoading', {});
-
-      });
-
-
-
-      return axios(requestConfig);
-
-    }
-
-
-
-    return Promise.reject(error);
-
-  },
-
 );
-
-
-
-api.interceptors.request.use(
-
-  (config) =>
-
-    getUser()
-
-      .then((user) => {
-
-        user = JSON.parse(user);
-
-
-
-        if (user && user.token)
-
-          config.headers.Authorization = `Bearer ${user.token}`;
-
-
-
-        return Promise.resolve(config);
-
-      })
-
-
-
-      .catch((error) => {
-
-        console.log(error);
-
-
-
-        return Promise.resolve(config);
-
-      }),
-
-
-
-  (error) => Promise.reject(error),
-
-);
-
-
 
 export default api;
-
