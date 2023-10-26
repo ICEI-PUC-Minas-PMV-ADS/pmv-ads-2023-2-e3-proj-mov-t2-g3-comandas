b2ceb04263eaf523a63b702665321873aaa1f576
alvarosoares01@gmail.com <alvarosoares01@gmail.com>
2023-10-26 19:49:24 -0300

    register height

--- src/appCustomer/src/screens/Register/CheckinRegister.jsx
+++ src/appCustomer/src/screens/Register/CheckinRegister.jsx
@@ -1,12 +1,5 @@
 import React from 'react';
-import {
-  StyleSheet,
-  Text,
-  View,
-  Pressable,
-  ScrollView,
-  SafeAreaView,
-} from 'react-native';
+import { StyleSheet, Text, View, Pressable } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import LottieView from 'lottie-react-native';
 import * as Animatable from 'react-native-animatable';
@@ -18,48 +11,45 @@ export default function CheckinRegister() {
   const navigation = useNavigation();
 
   return (
-   // <SafeAreaView style={{ flex: 1 }}>
-      <ScrollView style={styles.container}>
-        <View style={styles.containerLogo}>
-          <Animatable.Image
-            style={styles.image}
-            delay={100}
-            animation="flipInY"
-            source={Logo}
-            resizeMode="contain"
-          />
-        </View>
+    <View style={styles.container}>
+      <View style={styles.containerLogo}>
+        <Animatable.Image
+          style={styles.image}
+          delay={100}
+          animation="flipInY"
+          source={Logo}
+          resizeMode="contain"
+        />
+      </View>
 
+      <Animatable.View
+        delay={700}
+        animation="fadeInUp"
+        style={styles.containerForm}
+      >
+        <LottieView
+          style={styles.checkInLottie}
+          source={Checkin}
+          autoPlay
+          loop={false}
+          marginTop={10}
+        />
+        <Text style={styles.textForm}>Conta criada com Sucesso</Text>
         <Animatable.View
-          delay={700}
+          delay={2000}
           animation="fadeInUp"
-          style={styles.containerForm}
+          onAnimationEnd={() => navigation.navigate('Login')}
+          style={styles.footer}
         >
-          <LottieView
-            style={styles.checkInLottie}
-            source={Checkin}
-            autoPlay
-            loop={false}
-            marginTop={10}
-          />
-          <Text style={styles.textForm}>Conta criada com Sucesso</Text>
-          <Animatable.View
-            delay={2000}
-            animation="fadeInUp"
-            onAnimationEnd={() => navigation.navigate('Login')}
-            style={styles.footer}
-          >
-            <Text style={styles.textFooter}>
-              Caso não seja redirecionado para a tela de Login em alguns
-              instantes
-            </Text>
-            <Pressable onPress={() => navigation.navigate('Login')}>
-              <Text style={styles.textFooterLink}>Clique Aqui.</Text>
-            </Pressable>
-          </Animatable.View>
+          <Text style={styles.textFooter}>
+            Caso não seja redirecionado para a tela de Login em alguns instantes
+          </Text>
+          <Pressable onPress={() => navigation.navigate('Login')}>
+            <Text style={styles.textFooterLink}>Clique Aqui.</Text>
+          </Pressable>
         </Animatable.View>
-      </ScrollView>
-  //  </SafeAreaView>
+      </Animatable.View>
+    </View>
   );
 }
 
@@ -75,7 +65,6 @@ const styles = StyleSheet.create({
     opacity: 0.5,
   },
   image: {
-    // height: 120,
     alignSelf: 'center',
     height: 75,
     width: 75,
