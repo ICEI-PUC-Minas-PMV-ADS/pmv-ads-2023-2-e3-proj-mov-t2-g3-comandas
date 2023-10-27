23898febfe82284b526f3218951b0d00bf1f8483
alvarosoares01@gmail.com <alvarosoares01@gmail.com>
2023-10-27 16:08:14 -0300

    Search colors

---
+++ src/appCustomer/src/screens/Shop/index.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import {
+  ScrollView,
+  SafeAreaView,
+  StyleSheet,
+  Image,
+  Text,
+  TextInput,
+  View,
+  TouchableOpacity,
+  ActivityIndicator,
+  FlatList,
+} from 'react-native';
+
+export default function Shop() {
+  return (
+    <SafeAreaView>
+      <ScrollView>
+        <View />
+      </ScrollView>
+    </SafeAreaView>
+  );
+}
