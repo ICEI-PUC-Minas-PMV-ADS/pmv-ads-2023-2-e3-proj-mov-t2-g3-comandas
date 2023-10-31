0af85046db6ef2a5216e054f2174f4a346a9d28f
creinis <115181527+creinis@users.noreply.github.com>
2023-10-31 16:19:32 +0100

    add: create user address

--- src/appCustomer/src/context/LocationContext.jsx
+++ src/appCustomer/src/context/LocationContext.jsx
@@ -1,37 +1,37 @@
 import React, { createContext, useState, useContext, useEffect } from 'react';
 import {
-  getCurrentPositionAsync,
-  requestForegroundPermissionsAsync,
+    getCurrentPositionAsync,
+    requestForegroundPermissionsAsync,
 } from 'expo-location';
 
 export const LocationContext = createContext();
 
 export default function LocationProvider({ children }) {
-  const [location, setLocation] = useState(null);
+    const [location, setLocation] = useState(null);
 
-  async function requestLocationPermissions() {
-    const { granted } = await requestForegroundPermissionsAsync();
+    async function requestLocationPermissions() {
+        const { granted } = await requestForegroundPermissionsAsync();
 
-    if (granted) {
-      const currentLocation = await getCurrentPositionAsync();
-      setLocation(currentLocation);
+        if (granted) {
+            const currentLocation = await getCurrentPositionAsync();
+            setLocation(currentLocation);
+        }
     }
-  }
 
-  useEffect(() => {
-    requestLocationPermissions();
-  }, []);
+    useEffect(() => {
+        requestLocationPermissions();
+    }, []);
 
-  return (
-    // eslint-disable-next-line react/jsx-no-constructed-context-values
-    <LocationContext.Provider value={{ location, setLocation }}>
-      {children}
-    </LocationContext.Provider>
-  );
+    return (
+        // eslint-disable-next-line react/jsx-no-constructed-context-values
+        <LocationContext.Provider value={{ location, setLocation }}>
+            {children}
+        </LocationContext.Provider>
+    );
 }
 
 export function useLocation() {
-  const context = useContext(LocationContext);
-  const { location, setLocation } = context;
-  return { location, setLocation };
+    const context = useContext(LocationContext);
+    const { location, setLocation } = context;
+    return { location, setLocation };
 }
