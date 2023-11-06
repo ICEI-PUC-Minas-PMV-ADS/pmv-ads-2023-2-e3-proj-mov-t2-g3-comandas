0aefc31fecf86355a1a253de05d3f2517740a13c
alvarosoares01@gmail.com <alvarosoares01@gmail.com>
2023-11-06 16:02:20 -0300

    Search fix and home adjustments

--- src/appCustomer/src/services/auth.service.js
+++ src/appCustomer/src/services/auth.service.js
@@ -1,45 +1,47 @@
 import API from './webapi.service';
 
 export async function register(param) {
-    try {
-        const { data } = await API.post(`/customer/create`, param);
-        return data;
-    } catch (error) {
-        console.log(
-            `ERROR IN register => [${
-                error?.response?.status
-            }]\n ${JSON.stringify(error?.response?.data, null, 2)}`,
-        );
-        throw error;
-    }
+  try {
+    const { data } = await API.post(`/customer/create`, param);
+    return data;
+  } catch (error) {
+    console.log(
+      `ERROR IN register => [${error?.response?.status}]\n ${JSON.stringify(
+        error?.response?.data,
+        null,
+        2,
+      )}`,
+    );
+    throw error;
+  }
 }
 
 export async function login(param) {
-    try {
-        const { data } = await API.post(`/user/login`, param);
-        return data;
-    } catch (error) {
-        console.log(
-            `ERROR IN login => [${error?.response?.status}]\n ${JSON.stringify(
-                error?.response?.data,
-                null,
-                2,
-            )}`,
-        );
-        throw error;
-    }
+  try {
+    const { data } = await API.post(`/user/login`, param);
+    return data;
+  } catch (error) {
+    console.log(
+      `ERROR IN login => [${error?.response?.status}]\n ${JSON.stringify(
+        error?.response?.data,
+        null,
+        2,
+      )}`,
+    );
+    throw error;
+  }
 }
 
 export async function registerUserAddress(param) {
-    try {
-        const { data } = await API.post(`/address/create`, param);
-        return data;
-    } catch (error) {
-        console.log(
-            `ERROR IN registerUserAddress => [${
-                error?.response?.status
-            }]\n ${JSON.stringify(error?.response?.data, null, 2)}`,
-        );
-        throw error;
-    }
+  try {
+    const { data } = await API.post(`/address/create`, param);
+    return data;
+  } catch (error) {
+    console.log(
+      `ERROR IN registerUserAddress => [${
+        error?.response?.status
+      }]\n ${JSON.stringify(error?.response?.data, null, 2)}`,
+    );
+    throw error;
+  }
 }
