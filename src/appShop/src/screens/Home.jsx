87cf0c0ae132c09c5f314f03eb9d7b12f3f7eaff
Vitor <email.vitors@gmail.com>
2023-11-15 20:02:04 -0300

    new appShop

---
+++ src/appShop/src/screens/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { SafeAreaView, StyleSheet } from 'react-native';
+
+export default function Home() {
+    return (
+        <SafeAreaView style={styles.container}>
+        </SafeAreaView>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#FFF',
+    },
+});
